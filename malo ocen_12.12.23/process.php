ac40b3cbbc452c181ec029be6ffb9f69f9f8513b
Ihor <136530531+ihorhrychyna@users.noreply.github.com>
2023-12-12 11:23:55 +0100

    Add files via upload

---
+++ malo ocen_12.12.23/process.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "simple_project";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+
+    $sql = "INSERT INTO users (username, email) VALUES ('$username', '$email')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
